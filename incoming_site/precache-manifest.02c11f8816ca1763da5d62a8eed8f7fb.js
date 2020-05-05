3db3ab61b27bb2b468cc2d0be66fe533dacdc92a
Mateus Vidal <mateusvidal.dev@gmail.com>
2020-05-05 20:23:56 -0300

    Inclusão da build com nome na pasta

---
+++ incoming_site/precache-manifest.02c11f8816ca1763da5d62a8eed8f7fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28b30a9c54a5aa026e0548a3846575ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a29b5d216f1f6bf6a40",
+    "url": "/static/css/main.44ba3704.chunk.css"
+  },
+  {
+    "revision": "27cd401442bbb00b3543",
+    "url": "/static/js/2.e76f163d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e76f163d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a29b5d216f1f6bf6a40",
+    "url": "/static/js/main.ce75e411.chunk.js"
+  },
+  {
+    "revision": "7eb575652b51f369690c",
+    "url": "/static/js/runtime-main.fa60fd1d.js"
+  },
+  {
+    "revision": "3c32985f8ae21d8a8a2975789cdeb0a7",
+    "url": "/static/media/space_dev.3c32985f.gif"
+  }
+]);
